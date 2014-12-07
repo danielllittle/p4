afe9a28b740b85c7a661721a78f9953ef9801a06
Daniel Little <little@fas.harvard.edu>
2014-12-07 15:09:15 -0800

    add User Table, Migration & Seeder

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		DB::table('users')->delete();
+        User::create(array('password' => Hash::make('cheese'), 'username' => 'Mickey', 'email' => 'mickey@home.com'));
+        User::create(array('password' => Hash::make('quack'), 'username' => 'Donald', 'email' => 'donald@duckdynasty.com'));
+        User::create(array('password' => Hash::make('cheese'), 'username' => 'Mickey', 'email' => 'mickey@home.com'));
+        User::create(array('password' => Hash::make('cheese'), 'username' => 'Mickey', 'email' => 'mickey@home.com'));
+        User::create(array('password' => Hash::make('cheese'), 'username' => 'Mickey', 'email' => 'mickey@home.com'));
+        User::create(array('password' => Hash::make('cheese'), 'username' => 'Mickey', 'email' => 'mickey@home.com'));
+
+	}
+
+}
