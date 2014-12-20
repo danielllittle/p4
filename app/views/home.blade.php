5c5158eb58dee5fa0b8ef18188b7367ca756d864
Daniel Little <little@fas.harvard.edu>
2014-12-20 14:23:17 -0800

    add welcome verbage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -8,6 +8,7 @@
   <h1>Welcome to ParkAdvisor!</h1>
 
   <hr>
+  Park wait times are now available to everyone.   Have a better experience and avoid those long lines!
 <h3> {{HTML::linkAction('UserController@getLogin', 'Login')}} to access wait times!</h3>
 
 @stop
